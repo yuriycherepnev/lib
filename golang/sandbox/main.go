110fe2fa235ac5b6a11e666b8004a38a3a2fb5f9
yuriy <yuriy.cherepnev@gmail.com>
2024-10-11 10:16:20 +0300

    aloha dance

--- golang/sandbox/main.go
+++ golang/sandbox/main.go
@@ -1,51 +1,47 @@
+//с числом работаем как со строкой!
+//в типе инт не хватает длины цифры для обработки всего числа
+
 package main
 
 import (
 	"fmt"
 	"strconv"
-	"unicode/utf8"
 )
 
 func main() {
-	salary := 30101023
+	salary := 12345678
 	fmt.Println(salary)
 	cutSalary := cutSalary(salary)
 	fmt.Println(cutSalary)
 }
 
 func cutSalary(salary int) int {
-	cutSalary := 0
-	multiplier := 10
 	textNumber := strconv.Itoa(salary)
-	sliceSalary := make([]int, 0, utf8.RuneCountInString(textNumber))
-	minNumber := 9
-	minIndex := 0
-	currentIndex := 0
+	//sliceSalary := make([]string, 0, utf8.RuneCountInString(textNumber))
+	//minNumber := 9
+	//minIndex := 0
+	//currentIndex := 0
 
-	for salary > 0 {
-		currentNumber := salary % 10
-		sliceSalary = append(sliceSalary, currentNumber)
-		salary = salary / 10
-
-		if currentNumber <= minNumber {
-			minNumber = currentNumber
-			minIndex = currentIndex
-		}
-		if salary != 0 {
-			currentIndex++
-		}
+	for i, v := range textNumber {
+		fmt.Println(i)
+		fmt.Println(v)
 	}
 
-	sliceSalary = append(sliceSalary[:minIndex], sliceSalary[minIndex+1:]...)
+	//for salary > 0 {
+	//	currentNumber := salary % 10
+	//	sliceSalary = append(sliceSalary, currentNumber)
+	//	salary = salary / 10
+	//
+	//	if currentNumber <= minNumber {
+	//		minNumber = currentNumber
+	//		minIndex = currentIndex
+	//	}
+	//	if salary != 0 {
+	//		currentIndex++
+	//	}
+	//}
+	//
+	//sliceSalary = append(sliceSalary[:minIndex], sliceSalary[minIndex+1:]...)
 
-	for index, value := range sliceSalary {
-		if index == 0 {
-			multiplier = 1
-		} else {
-			multiplier = multiplier * 10
-		}
-		cutSalary = value*multiplier + cutSalary
-	}
-
-	return cutSalary
+	return 10
 }
